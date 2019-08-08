da09832584676f199263256479fa7db6120ad3e8
ColMugX <colmugx@qq.com>
2019-08-08 18:17:29 +0800

    chore: 更新 cli 配置 (#39)

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,20 +2,24 @@ const {VueLoaderPlugin} = require('vue-loader')
 const path = require('path')
 const glob = require('glob')
 
-const demos = ['docs/basic.md', ...glob.sync('docs/!(basic|faq).md')]
-const demoSections = demos.map(filePath => ({
-  name: path.basename(filePath, '.md'),
-  content: filePath
-}))
-
-module.exports = {
-  styleguideDir: 'docs',
-  pagePerSection: true,
-  ribbon: {
-    url: 'https://github.com/FEMessage/data-list'
-  },
-  require: ['./styleguide/global.requires.js'],
-  sections: [
+const sections = (() => {
+  const docs = glob
+    .sync('docs/*.md')
+    .map(p => ({name: path.basename(p, '.md'), content: p}))
+  const demos = []
+  let faq = '' // 约定至多只有一个faq.md
+  const guides = []
+  docs.forEach(d => {
+    if (/^faq$/i.test(d.name)) {
+      d.name = d.name.toUpperCase()
+      faq = d
+    } else if (/^guide-/.test(d.name)) {
+      guides.push(d)
+    } else {
+      demos.push(d)
+    }
+  })
+  return [
     {
       name: 'Components',
       components: 'src/data-list.vue',
@@ -23,13 +27,22 @@ module.exports = {
     },
     {
       name: 'Demo',
-      sections: demoSections
+      sections: demos,
+      sectionDepth: 2
     },
-    {
-      name: 'FAQ',
-      content: 'docs/faq.md'
-    }
-  ],
+    ...(faq ? [faq] : []),
+    ...(guides.length ? [{name: 'Guide', sections: guides}] : [])
+  ]
+})()
+
+module.exports = {
+  styleguideDir: 'docs',
+  pagePerSection: true,
+  ribbon: {
+    url: 'https://github.com/FEMessage/data-list'
+  },
+  sections,
+  require: ['./styleguide/global.requires.js'],
   webpackConfig: {
     module: {
       rules: [
@@ -49,6 +62,10 @@ module.exports = {
         {
           test: /\.styl(us)?$/,
           loaders: ['vue-style-loader', 'css-loader', 'stylus-loader']
+        },
+        {
+          test: /\.(woff2?|eot|[ot]tf)(\?.*)?$/,
+          loader: 'file-loader'
         }
       ]
     },
